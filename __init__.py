e09b91ec71bbf1c19ef81f25fde65dc2a11e2be8
ManuelArto <manuelartociao@gmail.com>
2021-02-03 16:08:55 +0100

    First version completed

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
-"""KDE Services"""
+# -*- coding: utf-8 -*-
+
+"""Matching KDE Services"""
 
 import os
 from functools import reduce
@@ -8,33 +10,27 @@ __title__ = "KDE Services"
 __version__ = "0.1.0"
 __authors__ = "Arto Manuel"
 
-kde_services_path = "/usr/share/kservices5/"
+KDE_SERVICES_PATH = "/usr/share/kservices5/"
+
 def get_services(query_string):
-	files = [file for file in os.listdir(kde_services_path) if ".desktop" in file]
+	files = [file for file in os.listdir(KDE_SERVICES_PATH) if ".desktop" in file]
 	for file in files:
 		has_exec = False
-		with open(kde_services_path + file, 'r') as f:
+		with open(KDE_SERVICES_PATH + file, 'r') as f:
 			for line in f.readlines():
 				if "Exec=" in line:
 					has_exec = True
 				if "X-KDE-Keywords=" in line:
-					if reduce(lambda prev, query: prev and (query in line[15:]), query_string.split(' '), True) and has_exec:
+					if has_exec and reduce(lambda prev, query: prev and (query in line[15:]), query_string.split(' '), True):
 						yield file
 
 def get_service_data(service):
 	data = {"Exec": "", "Icon": "", "Name": "", "Comment": "", "X-KDE-Keywords": ""}
-	with open(kde_services_path + service) as file:
+	with open(KDE_SERVICES_PATH + service) as file:
 		for line in file.readlines():
-			if "Exec=" in line:
-				data["Exec"] = line.replace("Exec=", "").strip()
-			elif "Icon=" in line:
-				data["Icon"] = iconLookup(line.replace("Icon=", "").strip())
-			elif "Name=" in line: 
-				data["Name"] = line.replace("Name=", "").strip()
-			elif "Comment=" in line: 
-				data["Comment"] = line.replace("Comment=", "").strip()
-			elif "X-KDE-Keywords=" in line:
-				data["X-KDE-Keywords"] = line.replace("X-KDE-Keywords=", "").strip()
+			for key in data.keys():
+				if key+"=" in line:
+					data[key] = line.replace(key+"=", "").strip() 
 	return data
 
 def handleQuery(query):
@@ -44,7 +40,7 @@ def handleQuery(query):
 			service_data = get_service_data(service)
 			items.append(Item(
 				id=service_data["Name"],
-				icon=service_data["Icon"],
+				icon=iconLookup(service_data["Icon"]),
 				text=service_data["Name"],
 				subtext=service_data["Comment"],
 				actions=[
