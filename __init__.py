ccfa4e725d6195187fcb3901733bdba4858c75fa
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2021-03-24 17:02:05 +0100

    fixed exec command and formatted

--- __init__.py
+++ __init__.py
@@ -7,35 +7,35 @@ from functools import reduce
 from albert import *
 
 __title__ = "KDE Services"
-__version__ = "0.1.0"
+__version__ = "0.2.1"
 __authors__ = "Arto Manuel"
 
 KDE_SERVICES_PATH = "/usr/share/kservices5/"
 
-def get_services(query_string) -> list:
-	services: list = []
+def get_services_path(query: str) -> list:
+	services = []
 	files = [file for file in os.listdir(KDE_SERVICES_PATH) if ".desktop" in file]
-	for file in files:
-		with open(KDE_SERVICES_PATH + file, 'r', encoding="utf-8") as f:
+	for file_path in files:
+		with open(KDE_SERVICES_PATH + file_path, 'r', encoding="utf-8") as f:
 			for line in f.readlines():
 				if "X-KDE-Keywords=" in line:
-					if reduce(lambda prev, query: prev and (query in line[15:]), query_string.split(' '), True):
-						services.append(file)
+					if reduce(lambda prev, query: prev and (query in line.removeprefix("X-KDE-Keywords=").lower()), query.lower().split(' '), True):
+						services.append(file_path)
 	return services
 
 def get_service_data(service_path: str) -> dict:
-	data = {"Exec": "", "Icon": "", "Name": "", "Comment": "", "X-KDE-Keywords": ""}
+	data = {"Icon": "", "Name": "", "Comment": ""}
 	with open(KDE_SERVICES_PATH + service_path, "r", encoding="utf-8") as file:
 		for line in file.readlines():
 			for key in data.keys():
 				if key+"=" in line:
-					data[key] = line.replace(key+"=", "").strip() 
+					data[key] = line.replace(key+"=", "").strip()
 	return data
 
 def handleQuery(query):
 	if query.string:
 		items = []
-		services = get_services(query.string)
+		services = get_services_path(query.string)
 		for service_path in services:
 			service_data = get_service_data(service_path)
 			items.append(Item(
@@ -44,16 +44,15 @@ def handleQuery(query):
 				text=service_data["Name"],
 				subtext=service_data["Comment"],
 				actions=[
-					ProcAction(text=service_data["Exec"], commandline=service_data["Exec"].split())
+					ProcAction(text = f"kcmshell5 {service_path}", commandline=["kcmshell5", service_path])
 				]
 			))
 		return items
 
+
 if __name__ == "__main__":
-	services  = get_services("kwin")
-	for service in services:
-		service_data = get_service_data(service)
+	services  = get_services_path("kwin")
+	for service_path in services:
+		service_data = get_service_data(service_path)
 		print()
-		print(service_data)
-	else:
-		print("No services", services)
+		print(service_path, ":", service_data)
